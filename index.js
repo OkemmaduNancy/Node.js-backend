e00f552b01c0d01f40c39a8c506b0adac0ee70f2
Okemmadu Nancy <anurika.okemmadu@gmail.com>
2021-05-07 19:58:45 +0100

    file modified

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 //create express app
 const app = express();
 
 //parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+// Configuring the database
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
 
 //define a simple route
-app.get('/', (req, res) => {
-    res.json({"message": "Welcome to nodejs backend" });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Todo app" });
 });
 
-//listen for requests
-app.listen(4000)
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+
+
+// require('./app/routes/todo.routes.js')(app);
+
+// listen for requests
+app.listen(4000, () =>{
+    mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
+});
