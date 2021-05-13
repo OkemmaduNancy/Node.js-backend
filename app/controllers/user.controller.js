702f9a2c11c789d9ec614c0722065659245af192
Okemmadu Nancy <anurika.okemmadu@gmail.com>
2021-05-13 08:56:31 +0100

    modified file

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -17,7 +17,7 @@ exports.createUser = (req, res) => {
   User.find({ email: req.body.email })
   .then((result) => {
         if (result.length > 0) {
-            console.log(result);
+            
             return res.status(404).send({
                 message: "email already exist",
             });
